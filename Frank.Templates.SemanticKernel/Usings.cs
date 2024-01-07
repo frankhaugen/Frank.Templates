a4397202c1adec8502d4275700e54ef6ed92802f
Frank R. Haugen <frank.haugen@gmail.com>
2024-01-07 04:19:47 +0100

    Update SemanticKernel project files and namespaces
    Fixed invalid character issue in SemanticKernel.csproj and modified usings in Usings.cs to be global. Also refined classifications in the template.json file and renamed another json file.

--- Frank.Templates.SemanticKernel/Usings.cs
+++ Frank.Templates.SemanticKernel/Usings.cs
@@ -1,5 +1,5 @@
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.SemanticKernel;
-using Spectre.Console;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Logging;
+global using Microsoft.SemanticKernel;
+global using Spectre.Console;
